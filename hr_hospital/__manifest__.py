69e422c9fc3d7c84adf730f4d29c2a49e77e932b
sasha <sashalyt2002@gmail.com>
2024-10-13 02:41:36 +0300

    fisrt task4 commit

--- hr_hospital/__manifest__.py
+++ hr_hospital/__manifest__.py
@@ -15,21 +15,22 @@
     'data': [
         'security/ir.model.access.csv',
         'views/hr_hospital_menu.xml',
+        'wizard/hr_hospital_reappointment_doctor_wizard_views.xml',
+        'wizard/hr_hospital_disease_report_wizard_views.xml',
         'views/hr_hospital_doctor_views.xml',
-        'views/hr_hospital_patient_views.xml',
         'views/hr_hospital_patient_visit_views.xml',
+        'views/hr_hospital_patient_views.xml',
         'views/hr_hospital_disease_type_views.xml',
         'views/hr_hospital_diagnosis_views.xml',
         'data/hr_hospital_disease_type.xml',
         'views/hr_hospital_disease_category_views.xml',
-        'wizard/hr_hospital_reappointment_doctor_wizard_views.xml',
-        'wizard/hr_hospital_disease_report_wizard_views.xml'
 
     ],
     'demo': [
         'demo/hr_hospital_doctor_demo.xml',
         'demo/hr.hospital.patient.csv',
         'demo/hr_hospital_disease_category_demo.xml',
+        'demo/hr.hospital.patient.visit.csv',
 
     ],
 
